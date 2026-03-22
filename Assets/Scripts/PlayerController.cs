2f0ce882dfbfb3339fa84d4c5ec94abac69e3acb
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-03-22 18:26:18 +0000

    speedplayer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,6 +28,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
